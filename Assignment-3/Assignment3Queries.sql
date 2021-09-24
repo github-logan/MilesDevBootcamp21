396d792bc67e7c2eda65b26d99e22444cc016937
github-logan <lp58725@gmail.com>
2021-09-24 17:00:24 -0400

    completed Assignment3

--- Assignment-3/Assignment3Queries.sql
+++ Assignment-3/Assignment3Queries.sql
@@ -34,7 +34,34 @@ SELECT TOP 10 price FROM sales;
 
 -- #7 Write a query to return all the values stored in all Lookup Tables - Lookup tables are the tables we reference typically with just an ID and a name. This should be a dynamic combining of all of the tables
 
-'tbd: something to do with join and union'
+SELECT ownerid AS ID, name AS Name, 'Owner' AS Type FROM owners
+UNION
+SELECT tavernid, name, 'Tavern' FROM taverns
+UNION
+SELECT locationid, address, 'Location' FROM locations
+UNION
+SELECT roleid, name, 'Role' FROM roles
+UNION
+SELECT av_statusid, statusname, 'Availability Status' FROM availabilityStatuses
+UNION
+SELECT classid, class_name, 'Class' FROM classes
+UNION
+SELECT guestid, last_name, 'Guest' FROM guests
+UNION
+SELECT supplytypeid, type_name, 'Supply Type' FROM supplyTypes
+order by Type ASC
 
 
 -- #8 Write a query that returns Guest Classes with Levels and Generate a new column with a label for their level grouping (lvl 1-10, 10-20, etc)
+
+SELECT last_name, first_name, class_name, current_level,
+CASE
+	WHEN current_level < 2 THEN 'novice'
+    WHEN current_level < 4 Then 'apprentice'
+	WHEN current_level > 4 THEN 'master'
+    Else 'journeyman'
+	END AS Ranking
+FROM guests
+JOIN guestLevels on guests.guestid = guestLevels.guestID
+JOIN classes on classes.classid = guestLevels.classid
+ORDER by last_name ASC
